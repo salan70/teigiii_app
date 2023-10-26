95e9056d6e2461d7757ab78e059b043d81f2f0b4
Tetsuo.Oda <tetsuo21ad@gmail.com>
2023-10-26 21:35:07 +0900

    feat: :lipstick: タブバーを実装

--- lib/feature/user_profile/presentation/page/profile_page.dart
+++ lib/feature/user_profile/presentation/page/profile_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../auth/application/auth_state.dart';
+import '../../../definition/presentation/component/definition_list.dart';
+import '../../../definition/util/definition_feed_type.dart';
 import 'profile_widget.dart';
 
 @RoutePage()
@@ -14,15 +16,95 @@ class ProfilePage extends ConsumerWidget {
     // TODO(me): 雑に!使ってるが、問題ないか確認する
     final userId = ref.watch(userIdProvider)!;
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('プロフィール'),
-      ),
-      body:  Column(
-        children: [
-          ProfileWidget(userId: userId),
-        ],
+    return DefaultTabController(
+      length: 2,
+      child: SafeArea(
+        child: Scaffold(
+          body: NestedScrollView(
+            headerSliverBuilder: (BuildContext context, bool _) {
+              return <Widget>[
+                const SliverAppBar(
+                  forceElevated: true,
+                  floating: true,
+                  title: Text('プロフィール'),
+                ),
+                SliverList(
+                  delegate: SliverChildListDelegate(
+                    [ProfileWidget(userId: userId)],
+                  ),
+                ),
+                SliverPersistentHeader(
+                  pinned: true,
+                  delegate: _StickyTabBarDelegate(
+                    tabBar: TabBar(
+                      labelColor: Theme.of(context).colorScheme.primary,
+                      unselectedLabelColor:
+                          Theme.of(context).colorScheme.onSurface,
+                      labelStyle: Theme.of(context).textTheme.titleMedium,
+                      indicatorWeight: 3,
+                      indicatorSize: TabBarIndicatorSize.label,
+                      tabs: const [
+                        Tab(text: '投稿'),
+                        Tab(text: 'いいね'),
+                      ],
+                    ),
+                  ),
+                ),
+              ];
+            },
+            body: TabBarView(
+              children: [
+                DefinitionList(
+                  definitionFeedType: DefinitionFeedType.homeRecommend,
+                ),
+                DefinitionList(
+                  definitionFeedType: DefinitionFeedType.homeFollowing,
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
+  }
+}
+
+/// SliverPersistentHeaderDelegateが必要なため、SliverPersistentHeaderを
+/// 継承したクラスを作成
+/// 参考: https://zenn.dev/wakanao/articles/0ff4bc3f08f34a#_tabsection()について
+class _StickyTabBarDelegate extends SliverPersistentHeaderDelegate {
+  const _StickyTabBarDelegate({required this.tabBar});
+
+  final TabBar tabBar;
+
+  @override
+  double get minExtent => tabBar.preferredSize.height;
+
+  @override
+  double get maxExtent => tabBar.preferredSize.height;
+
+  @override
+  Widget build(
+    BuildContext context,
+    double shrinkOffset,
+    bool overlapsContent,
+  ) {
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.surface,
+        border: Border(
+          bottom: BorderSide(
+            color: Theme.of(context).colorScheme.outlineVariant,
+            width: 0.3,
+          ),
+        ),
+      ),
+      child: tabBar,
+    );
+  }
+
+  @override
+  bool shouldRebuild(_StickyTabBarDelegate oldDelegate) {
+    return tabBar != oldDelegate.tabBar;
   }
 }
