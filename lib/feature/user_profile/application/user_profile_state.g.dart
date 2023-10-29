2b1d8c78f9aa86a8f0cf978da618585cbb79bdd9
Tetsuo.Oda <tetsuo21ad@gmail.com>
2023-10-29 13:17:00 +0900

    refactor: :recycle: followingIdListをProviderで保持するようリファクタ

--- lib/feature/user_profile/application/user_profile_state.g.dart
+++ lib/feature/user_profile/application/user_profile_state.g.dart
@@ -282,5 +282,134 @@ class _IsFollowingProviderElement extends FutureProviderElement<bool>
   @override
   String get targetUserId => (origin as IsFollowingProvider).targetUserId;
 }
+
+String _$followingIdListHash() => r'222e7250ab09f20c2dadf1776f9dc515223b74c9';
+
+/// See also [followingIdList].
+@ProviderFor(followingIdList)
+const followingIdListProvider = FollowingIdListFamily();
+
+/// See also [followingIdList].
+class FollowingIdListFamily extends Family<AsyncValue<List<String>>> {
+  /// See also [followingIdList].
+  const FollowingIdListFamily();
+
+  /// See also [followingIdList].
+  FollowingIdListProvider call(
+    String targetUserId,
+  ) {
+    return FollowingIdListProvider(
+      targetUserId,
+    );
+  }
+
+  @override
+  FollowingIdListProvider getProviderOverride(
+    covariant FollowingIdListProvider provider,
+  ) {
+    return call(
+      provider.targetUserId,
+    );
+  }
+
+  static const Iterable<ProviderOrFamily>? _dependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get dependencies => _dependencies;
+
+  static const Iterable<ProviderOrFamily>? _allTransitiveDependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get allTransitiveDependencies =>
+      _allTransitiveDependencies;
+
+  @override
+  String? get name => r'followingIdListProvider';
+}
+
+/// See also [followingIdList].
+class FollowingIdListProvider extends FutureProvider<List<String>> {
+  /// See also [followingIdList].
+  FollowingIdListProvider(
+    String targetUserId,
+  ) : this._internal(
+          (ref) => followingIdList(
+            ref as FollowingIdListRef,
+            targetUserId,
+          ),
+          from: followingIdListProvider,
+          name: r'followingIdListProvider',
+          debugGetCreateSourceHash:
+              const bool.fromEnvironment('dart.vm.product')
+                  ? null
+                  : _$followingIdListHash,
+          dependencies: FollowingIdListFamily._dependencies,
+          allTransitiveDependencies:
+              FollowingIdListFamily._allTransitiveDependencies,
+          targetUserId: targetUserId,
+        );
+
+  FollowingIdListProvider._internal(
+    super._createNotifier, {
+    required super.name,
+    required super.dependencies,
+    required super.allTransitiveDependencies,
+    required super.debugGetCreateSourceHash,
+    required super.from,
+    required this.targetUserId,
+  }) : super.internal();
+
+  final String targetUserId;
+
+  @override
+  Override overrideWith(
+    FutureOr<List<String>> Function(FollowingIdListRef provider) create,
+  ) {
+    return ProviderOverride(
+      origin: this,
+      override: FollowingIdListProvider._internal(
+        (ref) => create(ref as FollowingIdListRef),
+        from: from,
+        name: null,
+        dependencies: null,
+        allTransitiveDependencies: null,
+        debugGetCreateSourceHash: null,
+        targetUserId: targetUserId,
+      ),
+    );
+  }
+
+  @override
+  FutureProviderElement<List<String>> createElement() {
+    return _FollowingIdListProviderElement(this);
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return other is FollowingIdListProvider &&
+        other.targetUserId == targetUserId;
+  }
+
+  @override
+  int get hashCode {
+    var hash = _SystemHash.combine(0, runtimeType.hashCode);
+    hash = _SystemHash.combine(hash, targetUserId.hashCode);
+
+    return _SystemHash.finish(hash);
+  }
+}
+
+mixin FollowingIdListRef on FutureProviderRef<List<String>> {
+  /// The parameter `targetUserId` of this provider.
+  String get targetUserId;
+}
+
+class _FollowingIdListProviderElement
+    extends FutureProviderElement<List<String>> with FollowingIdListRef {
+  _FollowingIdListProviderElement(super.provider);
+
+  @override
+  String get targetUserId => (origin as FollowingIdListProvider).targetUserId;
+}
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
