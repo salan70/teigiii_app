d09c649d6588c3e5b6e7640e6005e3d48c0ae745
Tetsuo.Oda <tetsuo21ad@gmail.com>
2023-10-30 17:40:14 +0900

    refactor: :recycle: リスト表示にListView.builderを使うよう修正

--- lib/feature/word/presentation/page/index_page/index_page.dart
+++ lib/feature/word/presentation/page/index_page/index_page.dart
@@ -23,30 +23,35 @@ class IndexPage extends StatelessWidget {
         ),
         child: ListView(
           children: [
-            const Divider(),
-            for (final initialMainGroup in InitialMainGroup.values)
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        initialMainGroup.label,
-                        style: Theme.of(context).textTheme.titleLarge,
-                      ),
-                      IconButton(
-                        alignment: Alignment.centerRight,
-                        iconSize: 20,
-                        color: Theme.of(context).colorScheme.onSurfaceVariant,
-                        icon: const Icon(CupertinoIcons.chevron_forward),
-                        onPressed: () {},
-                      ),
-                    ],
-                  ),
-                  const Divider(),
-                ],
-              ),
+            ListView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: InitialMainGroup.values.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          InitialMainGroup.values[index].label,
+                          style: Theme.of(context).textTheme.titleLarge,
+                        ),
+                        IconButton(
+                          alignment: Alignment.centerRight,
+                          iconSize: 20,
+                          color: Theme.of(context).colorScheme.onSurfaceVariant,
+                          icon: const Icon(CupertinoIcons.chevron_forward),
+                          onPressed: () {},
+                        ),
+                      ],
+                    ),
+                    const Divider(),
+                  ],
+                );
+              },
+            ),
           ],
         ),
       ),
