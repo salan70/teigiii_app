dddf673c255463149e0bf43eadccd00961499f2b
Tetsuo.Oda <tetsuo21ad@gmail.com>
2023-11-15 17:58:19 +0900

    feat: :hammer: お問合せフォームを実装

--- lib/util/constant/url.dart
+++ lib/util/constant/url.dart
@@ -4,3 +4,7 @@ const latestInformationPage =
 String userReportFormUrl(String targetUserId, String currentUserId) {
   return 'https://docs.google.com/forms/d/e/1FAIpQLSe_Y83WDspTgZIN0obOtFxpCM3IPskdPHkQ30Rtw-mrvRmSeg/viewform?usp=pp_url&entry.1412333435=$targetUserId&entry.399122039=$currentUserId';
 }
+
+String inquireFormUrl(String currentUserId) {
+  return 'https://docs.google.com/forms/d/e/1FAIpQLScVQ21a8-CtMRwD7Syy3AZfK07SpZQQCjYSuqNNvoA4g7rSsw/viewform?usp=pp_url&entry.2104212620=$currentUserId';
+}
