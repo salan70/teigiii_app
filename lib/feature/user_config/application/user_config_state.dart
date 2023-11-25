195057cb3d738b1053a49bf32fcf733f9628ef6d
Tetsuo.Oda <tetsuo21ad@gmail.com>
2023-11-25 13:30:03 +0900

    fix: :bug: アカウント削除直後のエラーを回避するため、userIdProvider を readするよう修正

--- lib/feature/user_config/application/user_config_state.dart
+++ lib/feature/user_config/application/user_config_state.dart
@@ -8,7 +8,7 @@ part 'user_config_state.g.dart';
 
 @Riverpod(keepAlive: true)
 Future<List<String>> mutedUserIdList(MutedUserIdListRef ref) async {
-  final userId = ref.watch(userIdProvider)!;
+  final userId = ref.read(userIdProvider)!;
   final userProfileDoc =
       await ref.read(userConfigRepositoryProvider).fetchUserConfig(userId);
 
