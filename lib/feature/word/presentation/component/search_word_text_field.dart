35656a9b5682f464ca65ebda4ee5ac2acc50ebd7
Tetsuo.Oda <tetsuo21ad@gmail.com>
2023-11-30 17:23:15 +0900

    refactor: :recycle: EnteredTextNotifierを削除

--- lib/feature/word/presentation/component/search_word_text_field.dart
+++ lib/feature/word/presentation/component/search_word_text_field.dart
@@ -1,36 +1,57 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../../../core/common_provider/entered_text_state.dart';
 import '../../../../core/router/app_router.dart';
 
-class SearchWordTextField extends ConsumerWidget {
+/// 語句を検索する用の [TextField]。
+class SearchWordTextField extends StatefulWidget {
   const SearchWordTextField({
     super.key,
     this.defaultText,
   });
 
+  /// 初期値として表示するテキスト。
   final String? defaultText;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final controller = TextEditingController(
-      text: defaultText,
-    );
+  State<SearchWordTextField> createState() => _SearchWordTextFieldState();
+}
 
+class _SearchWordTextFieldState extends State<SearchWordTextField> {
+  late final TextEditingController controller;
+  late bool isEmpty;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = TextEditingController(text: widget.defaultText);
+
+    isEmpty = controller.text.isEmpty;
+
+    controller.addListener(() {
+      setState(() {
+        isEmpty = controller.text.isEmpty;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return TextField(
       controller: controller,
       textInputAction: TextInputAction.search,
-      onChanged: ref
-          .read(enteredTextNotifierProvider(EnterField.searchWord).notifier)
-          .updateText,
       onSubmitted: (value) {
         if (value.isEmpty) {
           return;
         }
-        controller.text = defaultText ?? '';
+        controller.text = widget.defaultText ?? '';
         context.pushRoute(
           SearchWordResultRoute(searchWord: value),
         );
@@ -41,29 +62,15 @@ class SearchWordTextField extends ConsumerWidget {
           size: 20,
         ),
         prefixIconColor: Theme.of(context).colorScheme.onSurfaceVariant,
-        suffixIcon: Consumer(
-          builder: (context, ref, child) {
-            final enteredText =
-                ref.watch(enteredTextNotifierProvider(EnterField.searchWord));
-            return enteredText.isNotEmpty
-                ? GestureDetector(
-                    onTap: () {
-                      controller.clear();
-                      ref
-                          .read(
-                            enteredTextNotifierProvider(EnterField.searchWord)
-                                .notifier,
-                          )
-                          .clearText();
-                    },
-                    child: const Icon(
-                      CupertinoIcons.clear_thick_circled,
-                      size: 20,
-                    ),
-                  )
-                : const SizedBox.shrink();
-          },
-        ),
+        suffixIcon: isEmpty
+            ? const SizedBox.shrink()
+            : GestureDetector(
+                onTap: controller.clear,
+                child: const Icon(
+                  CupertinoIcons.clear_thick_circled,
+                  size: 20,
+                ),
+              ),
         suffixIconColor: Theme.of(context).colorScheme.onSurfaceVariant,
         hintText: '語句を検索',
         filled: true,
