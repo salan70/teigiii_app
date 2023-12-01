50ff466e07daff05f39240e80acc47bc1ef7e8fe
Tetsuo.Oda <tetsuo21ad@gmail.com>
2023-12-01 12:31:07 +0900

    refactor: :recycle: アイコン画像のWidgetを切り出し

--- lib/feature/user_profile/presentation/page/edit_profile/edit_profile_page.dart
+++ lib/feature/user_profile/presentation/page/edit_profile/edit_profile_page.dart
@@ -1,21 +1,17 @@
-import 'dart:io';
-
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:pull_down_button/pull_down_button.dart';
 
 import '../../../../../core/common_provider/dialog_controller.dart';
 import '../../../../../core/common_widget/avatar_network_image_widget.dart';
 import '../../../../../core/common_widget/dialog/confirm_dialog.dart';
 import '../../../../../core/common_widget/error_and_retry_widget.dart';
 import '../../../../../core/router/app_router.dart';
-import '../../../../../util/constant/default_text_for_ui.dart';
 import '../../../../../util/logger.dart';
 import '../../../../../util/mixin/presentation_mixin.dart';
 import '../../../application/user_profile_for_write_notifier.dart';
+import '../../component/changeable_profile_image.dart';
 
 @RoutePage()
 class EditProfilePage extends ConsumerWidget with PresentationMixin {
@@ -35,24 +31,6 @@ class EditProfilePage extends ConsumerWidget with PresentationMixin {
     return asyncUserProfileForWrite.when(
       data: (userProfileForWrite) {
         final canEdit = notifier.canEdit();
-        final avatarWidget = userProfileForWrite.croppedFile == null
-            ? AvatarNetworkImageWidget(
-                imageUrl: userProfileForWrite.profileImageUrl,
-                avatarSize: avatarSize,
-              )
-            : Container(
-                width: avatarSize.diameter,
-                height: avatarSize.diameter,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                    image: FileImage(
-                      File(userProfileForWrite.croppedFile!.path),
-                    ),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              );
 
         return Scaffold(
           appBar: AppBar(
@@ -62,13 +40,13 @@ class EditProfilePage extends ConsumerWidget with PresentationMixin {
                 // キーボードを閉じる
                 primaryFocus?.unfocus();
 
+                // 初期表示時から入力内容に変更がない場合、確認ダイアログを表示せずに画面を閉じる。
                 if (!notifier.isStateChanged()) {
-                  // 初期表示時から入力内容に変更がない場合、確認ダイアログを表示せずに画面を閉じる
                   await context.popRoute();
                   return;
                 }
 
-                // 確認ダイアログを表示
+                // 確認ダイアログを表示する。
                 ref.read(dialogControllerProvider.notifier).show(
                       ConfirmDialog(
                         confirmMessage: '入力した内容は保存されません。\nよろしいですか？',
@@ -123,79 +101,9 @@ class EditProfilePage extends ConsumerWidget with PresentationMixin {
               child: ListView(
                 children: [
                   const SizedBox(height: 24),
-                  InkWell(
-                    key: globalKey,
-                    onTap: () async {
-                      // 位置を取得
-                      final box = globalKey.currentContext?.findRenderObject()
-                          as RenderBox?;
-                      final position =
-                          box!.localToGlobal(Offset.zero) & const Size(400, 80);
-
-                      if (!context.mounted) {
-                        return;
-                      }
-
-                      await showPullDownMenu(
-                        context: context,
-                        position: position,
-                        items: [
-                          PullDownMenuItem(
-                            onTap: () async {
-                              await executeWithOverlayLoading(
-                                ref,
-                                action: () async => notifier
-                                    .pickAndCropImage(ImageSource.camera),
-                                errorLogMessage: '画像選択もしくは切り抜き時にエラーが発生。',
-                                errorToastMessage: defaultErrorToastText,
-                              );
-                            },
-                            title: '写真を撮る',
-                            icon: CupertinoIcons.camera_fill,
-                          ),
-                          PullDownMenuItem(
-                            onTap: () async {
-                              await executeWithOverlayLoading(
-                                ref,
-                                action: () async => notifier
-                                    .pickAndCropImage(ImageSource.gallery),
-                                errorLogMessage: '画像選択もしくは切り抜き時にエラーが発生。',
-                                errorToastMessage: defaultErrorToastText,
-                              );
-                            },
-                            title: 'アルバムから選択',
-                            icon: CupertinoIcons.photo_fill,
-                          ),
-                          PullDownMenuItem(
-                            isDestructive: true,
-                            onTap: () => notifier.updateCroppedFileState(null),
-                            title: 'もとの画像に戻す',
-                            icon: CupertinoIcons.trash_fill,
-                          ),
-                        ],
-                      );
-                    },
-                    child: Stack(
-                      alignment: AlignmentDirectional.center,
-                      children: [
-                        avatarWidget,
-                        Container(
-                          alignment: AlignmentDirectional.bottomEnd,
-                          // AvatarIconWidgetの右下に配置するため、
-                          // AvatarIconWidgetの直径と同じサイズのコンテナを作成
-                          width: avatarSize.diameter,
-                          height: avatarSize.diameter,
-                          child: Icon(
-                            CupertinoIcons.camera_fill,
-                            size: 28,
-                            color: Theme.of(context)
-                                .colorScheme
-                                .onSurface
-                                .withOpacity(0.8),
-                          ),
-                        ),
-                      ],
-                    ),
+                  ChangeableProfileImage(
+                    userProfileForWrite: userProfileForWrite,
+                    globalKey: globalKey,
                   ),
                   const SizedBox(height: 16),
                   TextFormField(
